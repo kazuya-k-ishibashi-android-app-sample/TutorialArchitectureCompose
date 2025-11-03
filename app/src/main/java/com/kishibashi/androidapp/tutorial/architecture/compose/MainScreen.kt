e27dbb6618e05ddfc183588a776a5b22a11de1d2
kazuya-k-ishibashi <kazuya.ishibashi.wn@gmail.com>
2025-11-03 23:19:43 +0900

    init (7): MainScreen初期化
    1. {package}/MainScreen.kt
    2. {package}/MainActivity.kt

---
+++ app/src/main/java/com/kishibashi/androidapp/tutorial/architecture/compose/MainScreen.kt
@@ -0,0 +1,39 @@
+package com.kishibashi.androidapp.tutorial.architecture.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kishibashi.androidapp.tutorial.architecture.compose.ui.theme.AppTheme
+
+@Composable
+fun MainScreen() {
+
+    Scaffold { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text("Android Architecture")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    AppTheme {
+        MainScreen()
+    }
+}
